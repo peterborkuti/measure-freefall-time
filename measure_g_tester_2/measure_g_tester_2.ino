6dbdd29f47481e477bd92362e11100de8c14ab24
Peter Borkuti <peter.borkuti@liferay.com>
2017-01-24 09:26:34 +0100

    tester 2 - not working

---
+++ measure_g_tester_2/measure_g_tester_2.ino
@@ -0,0 +1,47 @@
+#include <MsTimer2.h>
+
+// Switch on LED on pin 13 each second
+
+
+#define OPTO 6
+#define SWITCH 7
+
+volatile int c = 0;
+volatile  boolean output = HIGH;
+
+void flash() {
+  c++;
+  if (c % 2 == 1) {
+    digitalWrite(SWITCH, HIGH); //switch to default
+    digitalWrite(OPTO, LOW); //ballStarting
+    delay(100);
+    digitalWrite(OPTO, HIGH);
+  }
+  else {
+    for (int i = 0; i < 5; i++) {
+      digitalWrite(SWITCH, LOW); //ballStarting
+      delay(random(3,5));
+      digitalWrite(SWITCH, HIGH);
+      delay(random(3,5));
+    }
+    digitalWrite(SWITCH, LOW); //ballStop
+    //digitalWrite(OPTO, HIGH); //opto to default
+  }
+   
+  digitalWrite(13, output);
+  output = !output;
+}
+
+void setup() {
+  pinMode(OPTO, OUTPUT);
+  pinMode(SWITCH, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  digitalWrite(OPTO, HIGH);
+  digitalWrite(SWITCH, HIGH);
+  MsTimer2::set(1000, flash); // 500ms period
+  MsTimer2::start();
+}
+
+void loop() {
+}
